04cf01be075363633e8c8931a81cb50ac02a5a22
saymonp <saymon.t@outlook.com>
2020-10-30 22:57:10 -0300

    Carousel na página de poc

--- src/app/pages/poc/poc.component.ts
+++ src/app/pages/poc/poc.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poc.component.css']
 })
 export class PocComponent implements OnInit {
-
+  slideIndex: number = 1;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.showSlides(this.slideIndex);
+  }
+
+  showSlides(n) {
+    let i: number;
+    var slides = document.getElementsByClassName("mySlides") as any;
+    //let slides = document.querySelector("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {this.slideIndex = 1}    
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex-1].style.display = "block";  
+    dots[this.slideIndex-1].className += " active";
+  }
+  
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+  
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
   }
 
 }
